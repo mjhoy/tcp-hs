99c97a31102774434c974971b85b4d835c2cb8eb
Mikey Hoy <mjh@mjhoy.com>
2022-09-12 21:26:12 -0400

    get a lil test ffi workin

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 module Main where
 
+import           Foreign.C                      ( CChar(..)
+                                                , CInt(..)
+                                                )
+import           Foreign.C.String               ( castCharToCChar )
+import           Prelude                 hiding ( sin )
+
+-- testing!
+foreign import ccall "take_a_char" c_take_a_char :: CChar -> CInt
+takeAChar :: Char -> Int
+takeAChar c = fromIntegral $ c_take_a_char $ castCharToCChar c
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    let char = 'x'
+    print $ takeAChar char
---
+++ app/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int take_a_char(char a) {
+    printf("Hello from C!\n");
+    return a + 1;
+}
